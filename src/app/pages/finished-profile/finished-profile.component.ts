91b98f7e57874d9bca644c09a332d2a92db8205e
Mario Beltran <mariobeltran0699@gmail.com>
2023-12-12 04:28:34 -0600

    ✨ add page finish profile component

---
+++ src/app/pages/finished-profile/finished-profile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-finished-profile',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './finished-profile.component.html',
+  styleUrls: ['./finished-profile.component.scss']
+})
+export class FinishedProfileComponent {
+
+}
